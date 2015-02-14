114e6d16b19aa921497bb45554ce1c2c9e730e68
saulbaizman <saul.baizman@gmail.com>
2015-02-14 17:57:43 -0500

    Added demo files for javascript 1 lecture

---
+++ demo/20150714-javascript-1/demo.ini.php
@@ -0,0 +1,46 @@
+; Welcome to USERLAND.
+
+; This is a comment line and is ignored.
+; PHP ini-style configuration file. Modify this with any old text editor. It works like this:
+; setting = value
+; Don't change anything to the left of the "=" sign!
+
+DEMO_NAME = JavaScript 1
+
+DEMO_DESCRIPTION = These demos are related to chapters 1 - 3 in <em>JavaScript and jQuery</em> by Jon Duckett.
+
+file[] = 0001.html
+caption[] = "Use the write method of the document object to output a couple of words on screen."
+
+file[] = 0002.html
+caption[] = "Define a variable whose contents are output on screen via document write."
+
+file[] = 0003.html
+caption[] = "Add two numbers together and output the result."
+
+file[] = 0004.html
+caption[] = "Add / concatenate two strings together via the string operator and output the result."
+
+file[] = 0005.html
+caption[] = "Use document.getElementById to get the contents of an HTML element, then display it via the alert() function."
+
+file[] = 0006.html
+caption[] = "Use document.getElementById to get the contents of an HTML element, then display it in the console via the console.log() method. Handy for debugging."
+
+file[] = 0007.html
+caption[] = "Dynamically set the content of an HTML element via the innerHTML property."
+
+file[] = 0008.html
+caption[] = "Create an array and output a value of the array based on the index."
+
+file[] = 0009.html
+caption[] = "Define a function and execute that function twice."
+
+file[] = 0010.html
+caption[] = "Pass a variable to a function."
+
+file[] = 0011.html
+caption[] = "Return a value from a function."
+
+file[] = 0012.html
+caption[] = "The HTML file links to an external CSS file. In this demo, the link is in the HEAD section. Usually, the link is just above the closing body tag for performance reasons. Note: this demo won't run because it references an exernal file."
