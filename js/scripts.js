a8d354b817c573290dd2f2ffc2a0a81a96a5bdff
Saul Baizman <saul.baizman@gmail.com>
2015-04-10 11:29:52 -0400

    Added scroll-to-top functionality in the source code div when a new 'step' is selected.

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,10 @@ function loadSteps ( demo ) {
         // if we are switching demos, blank out the source code
         $('#source_code').css ({'opacity':'0'}) ;
 
-        // populate #steps div, fade it in ;
+        // populate #steps div, fade it in
         $steps_div.html(data).fadeTo(250,1) ;
 
-        // Add download link styles for steps.
+        // add download link styles for steps
         var $steps_li = $('#steps ol li') ;
         $steps_li.on ( 'mouseover', function () {
             var dl_link = '#' + this.id + '-download' ;
@@ -44,7 +44,7 @@ function loadSteps ( demo ) {
         var selected_demo = '#demo-' + demo ;
         $(selected_demo).addClass ( 'demo_selected' ) ;
 
-        // Scroll to the top of the steps.
+        // scroll to the top of the steps, if needed
         if ( $steps_div.scrollTop() != 0 ) {
             $steps_div.scrollTop(0) ;
         }
@@ -52,8 +52,10 @@ function loadSteps ( demo ) {
     }
 
     function recover ( ) {
+
         console.warn ( 'No data was returned from the server!' ) ;
         $('#steps').html( '<strong>There was an error retrieving data from the server.</strong>' ) ;
+
     }
 
 }
@@ -72,15 +74,15 @@ function loadStepDetails ( demo, step ) {
         // populate #source_code div
         // NOTE: this will need to be more complicated if it is to return the
         // language format automatically.
-        var source_code = $('#source_code') ;
+        var $source_code = $('#source_code') ;
 
         // set opacity to 0
-        source_code.css ({'opacity':'0'}) ;
+        $source_code.css ({'opacity':'0'}) ;
 
         // populate the #source_code div content, then fade it in
-        source_code.html(data).fadeTo(250,1) ;
+        $source_code.html(data).fadeTo(250,1) ;
 
-        // Because we are loading rainbow'd content after the DOM has loaded, we need to manually invoke it here to style the content correctly
+        // because we are loading rainbow'd content after the DOM has loaded, we need to manually invoke it here to style the content correctly
         Rainbow.color();
 
         // set the previously selected demo to not be selected
@@ -90,11 +92,18 @@ function loadStepDetails ( demo, step ) {
         var selected_step = 'li#li-' + demo + '-' + step.replace('.' ,'-') ;
         $(selected_step).addClass ( 'step_selected' ) ;
 
+        // scroll to the top of the source code, if needed
+        if ( $source_code.scrollTop() != 0 ) {
+            $source_code.scrollTop(0) ;
+        }
+
     }
 
     function recover ( ) {
+
         console.warn ( 'No data was returned from the server!' ) ;
         $('#source_code').html( '<strong>There was an error retrieving data from the server.</strong>' ) ;
+
     }
 
 }
